91f9e96d0970c8789e0ea2b885799256a30193bf
xXcarlos029Xx <2del2001@gmail.com>
2024-11-27 20:21:57 +0100

    actu

--- src/api/db.js
+++ src/api/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async() => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect("mongodb+srv://Carlos:ROOTroot@tfg.nqk85.mongodb.net/TFG");
         console.log("Base de Datos conectada");
     } catch (error) {
         console.log(error);
--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config(); 
+console.log("URI de conexión:", process.env.MONGO_URI);
 import app from './app.js';
 import {connectDB} from './db.js';
 

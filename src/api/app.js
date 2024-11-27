0e7014b1aa021e54bfc651d79128ff3e4711d0bf
xXcarlos029Xx <2del2001@gmail.com>
2024-11-27 21:47:16 +0100

    ac

--- src/api/app.js
+++ src/api/app.js
@@ -4,7 +4,11 @@ import ruta from './routes/rutas.js';
 import cors from 'cors';
 
 const app = express(); // Crear la instancia de express aquí
-app.use(cors());
+app.use(cors({
+    origin: '*', // Permitir cualquier origen (ajusta esto para producción)
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(morgan("dev"));
 app.use(express.json());
 

e92958b4010d42aa4a211b60bd32760baac9576d
xXcarlos029Xx <2del2001@gmail.com>
2024-11-27 20:48:43 +0100

    a

--- src/api/db.js
+++ src/api/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async() => {
     try {
-        await mongoose.connect("mongodb+srv://Carlos:ROOTroot@tfg.nqk85.mongodb.net/TFG");
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Base de Datos conectada");
     } catch (error) {
         console.log(error);
